7139308e1a6ed15f8c7f63b13b7b993fb64f8c25
Bryant Ortega <kalicorose@gmail.com>
2024-01-03 16:35:09 -0800

    fixed recipe list view and added detail template

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home
+from .views import RecipesListView
 
 app_name = 'recipes'
 
 urlpatterns = [
    path('', home),
+   path("recipes/", RecipesListView.as_view(), name="recipes_list"),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView  # to display lists
+from .models import Recipe  # to access Recipe model
 
 # Create your views here.
 
+class RecipesListView(ListView):           #class-based view
+   model = Recipe                         #specify model
+   template_name = 'recipes/recipes_list.html'    #specify template 
+
+class RecipeDetailView(DetailView):
+   model = Recipe
+   template_name = 'recipes/detail.html'
+
 def home(request):
   return render(request, 'recipes/recipes_home.html')
+
