2ec40504d3ef55467a6270d6ae459f08b2cf5471
Bryant Ortega <kalicorose@gmail.com>
2024-01-07 15:28:17 -0800

    fixed path issues

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home, records
 from .views import RecipesListView
 from .views import RecipeDetailView
@@ -12,3 +14,6 @@ urlpatterns = [
    path('recipes/<pk>', RecipeDetailView.as_view(), name='detail'),
    path('recipes/search/', records, name='recipe_search')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
