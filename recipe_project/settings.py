312d523fd1f95356399ccd68df841b4b24c4b3af
Bryant Ortega <kalicorose@gmail.com>
2024-01-07 16:20:07 -0800

    Update ALLOWED_HOSTS with site and development server URL

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['https://sleepy-bayou-27564']
 
 
 # Application definition
