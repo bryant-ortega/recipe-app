a1d44180d255205227722d5ce4dbbfe67a44bc67
Bryant Ortega <kalicorose@gmail.com>
2024-01-07 16:24:47 -0800

    updated allowed hosts

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://sleepy-bayou-27564']
+ALLOWED_HOSTS = ['sleepy-bayou-27564-8100e6ae4537.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
